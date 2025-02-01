c75ff2cb60ddf46e682c9b89e171ff8fd49d423c
Ulad <vladis67948@gmail.com>
2025-02-01 18:39:54 +0100

    feat(app): added aliases, import order

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	resolve: {
+		alias: {
+			'@api': path.resolve(__dirname, 'src/api'),
+			'@styles': path.resolve(__dirname, 'src/styles'),
+			'@types': path.resolve(__dirname, 'src/types'),
+			'@components': path.resolve(__dirname, 'src/components')
+		}
+	},
+	plugins: [react()]
 })
