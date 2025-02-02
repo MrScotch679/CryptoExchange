e0e52124f9f5771f99ba283615b822c5713911a3
Ulad <vladis67948@gmail.com>
2025-02-02 17:48:16 +0100

    feat(api): fixed proxy, added getters

--- src/api/CryptoAPI.ts
+++ src/api/CryptoAPI.ts
@@ -1,11 +1,26 @@
 import { ApiResponse } from 'types/ApiResponse'
+import { Crypto, CryptoId } from 'types/Crypto'
+import { CryptoRateResponse } from 'types/CryptoRate'
 
 import { apiClient } from './apiClient'
 
 export class CryptoAPI {
-	async getAvailableCryptos(): Promise<ApiResponse<Crypto>> {
-		const response = await apiClient.get('/v1/cryptocurrency/map')
+	async getAvailableCryptos(): Promise<ApiResponse<Crypto[]>> {
+		const response = await apiClient.get(
+			'/v1/cryptocurrency/map?start=1&limit=100'
+		)
 
-		return response.data.data
+		return response.data
+	}
+
+	async getCryptoExchangeRate(
+		sourceId: CryptoId,
+		targetId: CryptoId
+	): Promise<ApiResponse<CryptoRateResponse>> {
+		const response = await apiClient.get(
+			`/v2/cryptocurrency/quotes/latest?id=${sourceId},${targetId}`
+		)
+
+		return response.data
 	}
 }
--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
+const PROXY_URL = import.meta.env.VITE_PROXY_URL
 const API_URL = import.meta.env.VITE_API_URL
 const API_KEY = import.meta.env.VITE_API_KEY
 
 export const apiClient = axios.create({
-	baseURL: API_URL,
+	baseURL: `${PROXY_URL}/?${API_URL}`,
 	headers: {
 		'X-CMC_PRO_API_KEY': API_KEY,
 		'Content-Type': 'application/json'
