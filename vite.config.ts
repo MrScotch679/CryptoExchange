7021064137f42cd9990a5a08253313d2fc0952ed
Ulad <vladis67948@gmail.com>
2025-02-02 18:10:27 +0100

    feat(app): aliases, import order

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+	plugins: [react()],
 	resolve: {
 		alias: {
 			'@api': path.resolve(__dirname, 'src/api'),
 			'@styles': path.resolve(__dirname, 'src/styles'),
 			'@types': path.resolve(__dirname, 'src/types'),
-			'@components': path.resolve(__dirname, 'src/components')
+			'@store': path.resolve(__dirname, 'src/store'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@utils': path.resolve(__dirname, 'src/utils')
 		}
-	},
-	plugins: [react()]
+	}
 })
